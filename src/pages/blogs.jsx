834b5086a137408856de16ad7654ef7c6d24eeec
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-30 00:01:19 +0430

    Add show all blogs

--- src/pages/blogs.jsx
+++ src/pages/blogs.jsx
@@ -1,21 +1,51 @@
 import {
     Container,
-    Box,
-    Typography,
+    Grid,
 } from "@mui/material";
 
+import Axios from "axios";
+
+import {useState, useEffect} from "react";
+import BlogItem from "../components/blogitem";
+
 const BlogsPage = () => {
+    const [blogs, setBlogs] = useState([]);
+
+    useEffect(() => {
+        Axios.get('http://localhost:8000/blogs/all')
+            .then((result) => {
+                setBlogs(result.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }, [blogs]);
+
     return (
         <Container
             sx={{
                 pt: "1rem",
             }}
         >
-            <Box>
-                <Typography>
-                    Archive blogs page
-                </Typography>
-            </Box>
+            <Grid
+                spacing={3}
+                container
+            >
+                {
+                    blogs.map((blog) => {
+                        return (
+                            <Grid
+                                md={4}
+                                sm={6}
+                                xs={12}
+                                item
+                            >
+                                <BlogItem blog={blog} />
+                            </Grid>
+                        );
+                    })
+                }
+            </Grid>
         </Container>
     );
 }
