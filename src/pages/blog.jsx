b738300257c4cbfbd8fa8a462d06ddf6ecd87700
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-30 00:06:31 +0430

    Add loading

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -8,12 +8,13 @@ import {
     Box,
     Typography,
     Divider,
+    CircularProgress,
 } from "@mui/material";
 
 const BlogPage = () => {
     let {blog_id} = useParams();
 
-    const [blog, setBlog] = useState([]);
+    const [blog, setBlog] = useState(false);
 
     useEffect(() => {
         Axios.get(`http://localhost:8000/blogs/get/${blog_id}`)
@@ -32,26 +33,36 @@ const BlogPage = () => {
                 pt: "1rem",
             }}
         >
-            <Box>
-                <Typography
-                    variant="h4"
-                    color="primary"
-                    gutterBottom
-                >
-                    {blog.title}
-                </Typography>
-                <Typography
-                    paragraph
-                    gutterBottom
-                >
-                    {blog.content}
-                </Typography>
-                <Divider />
-                <br />
-                <Typography>
-                    Author: {blog.author}
-                </Typography>
-            </Box>
+            {
+                blog
+                ?
+                    <Box>
+                        <Typography
+                            variant="h4"
+                            color="primary"
+                            gutterBottom
+                        >
+                            {blog.title}
+                        </Typography>
+                        <Typography
+                            paragraph
+                            gutterBottom
+                        >
+                            {blog.content}
+                        </Typography>
+                        <Divider />
+                        <br />
+                        <Typography>
+                            Author: {blog.author}
+                        </Typography>
+                    </Box>
+                :
+                    <Box
+                        sx={{ textAlign: "center" }}
+                    >
+                        <CircularProgress />
+                    </Box>
+            }
         </Container>
     );
 }
