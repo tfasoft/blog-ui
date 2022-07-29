e00a6cfa9386f9ea25220f2465baf821c41df4d9
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-29 15:04:59 +0430

    Add routes and etc

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import {ThemeProvider, createTheme, CssBaseline, colors as Colors} from "@mui/material";
+
+import Navbar from "./components/navbar";
+
+import AuthenticationPage from "./pages/authentication";
+import BlogPage from "./pages/blog";
+import BlogsPage from "./pages/blogs";
+import HomePage from "./pages/home";
+
 function App() {
-  return (
-    <h1>Hi</h1>
-  );
+    const theme = createTheme({
+        palette: {
+            primary: {
+                main: Colors.red[900],
+            },
+        },
+    });
+
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline/>
+            <Router>
+                <Navbar text="Bye">Hello</Navbar>
+                <Switch>
+                    <Route path="/" exact><HomePage /></Route>
+                    <Route path="/auth" exact><AuthenticationPage/></Route>
+                    <Route path="/blogs" exact><BlogsPage/></Route>
+                    <Route path="/blog/:blogid" exact><BlogPage/></Route>
+                </Switch>
+            </Router>
+        </ThemeProvider>
+    );
 }
 
 export default App;
