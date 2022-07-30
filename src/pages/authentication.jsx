a80929220fc4a6b2f24b80447f27f3acacd62966
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-30 16:45:22 +0430

    Done in auth

--- src/pages/authentication.jsx
+++ src/pages/authentication.jsx
@@ -1,5 +1,6 @@
 import {useState} from "react";
-import {useSelector} from "react-redux";
+import {useHistory} from "react-router-dom";
+import {useSelector, useDispatch} from "react-redux";
 
 import {
     Container,
@@ -9,18 +10,55 @@ import {
     CardContent,
     TextField,
     Button,
+    Snackbar,
+    Alert,
 } from "@mui/material";
 
 import Axios from "axios";
 
+import {setUID} from "../redux/actions/uid";
+import {logoinUser} from "../redux/actions/session";
+import {createUser} from "../redux/actions/user";
+
 const AuthenticationPage = () => {
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    const session = useSelector(state => state.session);
+    if (session) history.push('/auth');
+
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+
+    const [snackOpen, setSnackOpen] = useState(false);
+    const [snackTitle, setSnackTitle] = useState('');
+    const [snackType, setSnackType] = useState('');
+    const createSnack = (title, type) => {
+        setSnackTitle(title);
+        setSnackType(type);
+
+        setSnackOpen(true);
+    }
 
     const theme = useSelector(state => state.theme);
 
     const login = () => {
-        Axios.post();
+        const data = {
+            username,
+            password,
+        }
+
+        Axios.post('http://localhost:8000/auth/login', data)
+            .then((result) => {
+                const author = result.data.author;
+
+                dispatch(setUID(author._id));
+                dispatch(logoinUser());
+                dispatch(createUser(author));
+            })
+            .catch((error) => {
+                createSnack(error.response.data.message, 'error');
+            });
     }
 
     return (
@@ -69,6 +107,7 @@ const AuthenticationPage = () => {
                             size="small"
                             placeholder="Enter password"
                             label="Password"
+                            type="password"
                             sx={{ mb: "1rem" }}
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
@@ -76,6 +115,7 @@ const AuthenticationPage = () => {
                         />
                         <Button
                             variant="contained"
+                            onClick={() => login()}
                             disableElevation
                             fullWidth
                         >
@@ -84,6 +124,12 @@ const AuthenticationPage = () => {
                     </CardContent>
                 </Card>
             </Container>
+
+            <Snackbar open={snackOpen} autoHideDuration={6000} onClose={() => setSnackOpen(false)}>
+                <Alert onClose={() => setSnackOpen(false)} severity={snackType}>
+                    {snackTitle}
+                </Alert>
+            </Snackbar>
         </Container>
     )
 }
