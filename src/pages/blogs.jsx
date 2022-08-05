4d7d1864739aee2b69673e4766e6f1c87855fe33
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-05 06:34:59 +0430

    Add meta tag to blogs

--- src/pages/blogs.jsx
+++ src/pages/blogs.jsx
@@ -1,4 +1,10 @@
+import {useState, useEffect} from "react";
 import {useSelector} from "react-redux";
+
+import Axios from "axios";
+
+import DocumentMeta from 'react-document-meta';
+
 import {
     Container,
     Grid,
@@ -7,9 +13,6 @@ import {
     Typography, Toolbar,
 } from "@mui/material";
 
-import Axios from "axios";
-
-import {useState, useEffect} from "react";
 import BlogItem from "../components/blogitem";
 
 const BlogsPage = () => {
@@ -30,65 +33,82 @@ const BlogsPage = () => {
 
     const theme = useSelector(state => state.theme);
 
-    return (
-        <Container
-            maxWidth="xl"
-            sx={{
-                mb: "5rem",
-                background: `linear-gradient(to bottom, ${ theme === "light" ? "#071e4e" : "#222" } 60%, ${ theme === "light" ? "#f8f4fc" : "#333" } 40%)`,
-            }}
-        >
-            <Toolbar />
-            {
-                blogs
-                ?
-                    <Container
-                        sx={{
-                            mt: "5rem",
-                        }}
-                    >
-                        <Typography
-                            variant="h2"
-                            fontWeight="bold"
-                            color="white"
-                            gutterBottom
-                        >
-                            Featured posts
-                        </Typography>
-                        <Grid
-                            spacing={3}
-                            container
-                        >
-                            {
-                                blogs.reverse().map((blog) => {
-                                    return (
-                                        <Grid
-                                            md={4}
-                                            sm={6}
-                                            xs={12}
-                                            item
-                                        >
-                                            <BlogItem blog={blog} />
-                                        </Grid>
-                                    );
-                                })
-                            }
-                        </Grid>
-                    </Container>
-                :
-                    <Box
-                        sx={{
-                            textAlign: "center"
-                        }}
-                    >
-                        <CircularProgress
-                            sx={{
-                                mt: "3rem",
-                            }}
-                        />
-                    </Box>
+    const meta = {
+        title: "Read blogs",
+        description: 'Every posted blog is now available here.',
+        canonical: 'https://blog.amirhossein.info/blogs',
+        meta: {
+            charset: 'utf-8',
+            name: {
+            keywords: 'tfasoft,tfasoft blog,blog,tfa'
             }
-        </Container>
+        },
+        link: {
+            rel: { icon: "../assets/icons/favicon.ico" },
+        },
+    }
+
+    return (
+        <DocumentMeta {...meta}>
+            <Container
+                maxWidth="xl"
+                sx={{
+                    mb: "5rem",
+                    background: `linear-gradient(to bottom, ${ theme === "light" ? "#071e4e" : "#222" } 60%, ${ theme === "light" ? "#f8f4fc" : "#333" } 40%)`,
+                }}
+            >
+                <Toolbar />
+                {
+                    blogs
+                    ?
+                        <Container
+                            sx={{
+                                mt: "5rem",
+                            }}
+                        >
+                            <Typography
+                                variant="h2"
+                                fontWeight="bold"
+                                color="white"
+                                gutterBottom
+                            >
+                                Featured posts
+                            </Typography>
+                            <Grid
+                                spacing={3}
+                                container
+                            >
+                                {
+                                    blogs.reverse().map((blog) => {
+                                        return (
+                                            <Grid
+                                                md={4}
+                                                sm={6}
+                                                xs={12}
+                                                item
+                                            >
+                                                <BlogItem blog={blog} />
+                                            </Grid>
+                                        );
+                                    })
+                                }
+                            </Grid>
+                        </Container>
+                    :
+                        <Box
+                            sx={{
+                                textAlign: "center"
+                            }}
+                        >
+                            <CircularProgress
+                                sx={{
+                                    mt: "3rem",
+                                }}
+                            />
+                        </Box>
+                }
+            </Container>
+        </DocumentMeta>
     );
 }
 
