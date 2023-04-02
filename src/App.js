a1861350b5c415d6b9d6329e30539254b52c5b78
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-04-02 18:18:16 +0330

    Files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import {ThemeProvider, createTheme, CssBaseline, colors as Colors} from "@mui/material";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { ThemeProvider, createTheme, CssBaseline } from "@mui/material";
 
-import {useSelector, useDispatch} from "react-redux";
-import {envCreate} from "./redux/actions/env";
+import { useSelector } from "react-redux";
 
-import Navbar from "./components/navbar";
+import { Navbar } from "./components";
 
 import AuthenticationPage from "./pages/authentication";
 import BlogPage from "./pages/blog";
@@ -13,52 +12,59 @@ import HomePage from "./pages/home";
 import PanelPage from "./pages/panel";
 
 function App() {
-    const dispatch = useDispatch();
-    const mode = useSelector(state => state.theme);
+  const mode = useSelector((state) => state.theme);
 
-    dispatch(envCreate(process.env));
-
-    const theme = createTheme({
-        palette: {
-            mode: mode,
-            background: {
-                default: mode === "light" ? "#f8f4fc" : "#333",
-                paper: mode === "light" ? "#fff" : "#222",
-            },
-            primary: {
-                main: mode === "light" ? "#071e4e" : "#fff",
-            },
+  const theme = createTheme({
+    palette: {
+      mode: mode,
+      background: {
+        default: mode === "light" ? "#f8f4fc" : "#333",
+        paper: mode === "light" ? "#fff" : "#222",
+      },
+      primary: {
+        main: mode === "light" ? "#071e4e" : "#fff",
+      },
+    },
+    components: {
+      MuiPaperTable: {
+        defaultProps: {
+          sx: {
+            border: "none",
+            borderRadius: 5,
+          },
+          variant: "outlined",
         },
-        components: {
-            MuiPaperTable: {
-                defaultProps: {
-                    sx: {
-                        border: "none",
-                        borderRadius: 5,
-                    },
-                    variant: "outlined"
-                }
-            }
-        }
-    });
+      },
+    },
+  });
 
-    return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline/>
-            <Router>
-                <Navbar />
-                <Switch>
-                    <Route path="/" exact><HomePage/></Route>
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact>
+            <HomePage />
+          </Route>
 
-                    <Route path="/auth" exact><AuthenticationPage/></Route>
-                    <Route path="/panel" exact><PanelPage/></Route>
+          <Route path="/auth" exact>
+            <AuthenticationPage />
+          </Route>
+          <Route path="/panel" exact>
+            <PanelPage />
+          </Route>
 
-                    <Route path="/blogs" exact><BlogsPage/></Route>
-                    <Route path="/blog/:blog_id" exact><BlogPage/></Route>
-                </Switch>
-            </Router>
-        </ThemeProvider>
-    );
+          <Route path="/blogs" exact>
+            <BlogsPage />
+          </Route>
+          <Route path="/blog/:id" exact>
+            <BlogPage />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
+  );
 }
 
 export default App;
