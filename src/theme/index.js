ded92fc50aefa0eab82fa3833608085077a1dec7
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-04-04 23:42:36 +0330

    Add theme

---
+++ src/theme/index.js
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    background: {
+      default: "#f8f4fc",
+      paper: "#fff",
+    },
+    primary: {
+      main: "#071e4e",
+    },
+  },
+});
+
+export default theme;
