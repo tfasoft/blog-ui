5c593753e72625143e659885ea11f64e9b1ece3b
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-04-06 17:25:48 +0330

    Redux stuff

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,9 +1,9 @@
 import { combineReducers } from "redux";
 
-// import userReducer from "./reducers/user";
+import userReducer from "./reducers/user";
 import tokenReducer from "./reducers/token";
 
 export default combineReducers({
-  // user: userReducer,
+  user: userReducer,
   token: tokenReducer,
 });
