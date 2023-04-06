10d52d238fdda2d1061a3eeb28aae597129ddec0
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-04-06 17:56:22 +0330

    Fix

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { apiConfig } from "@/config";
 
 const API = axios.create({
-  baseURL: apiConfig.endpoint,
+  baseURL: apiConfig.endpoint || "http://localhost:4444/api",
 });
 
 export default API;
